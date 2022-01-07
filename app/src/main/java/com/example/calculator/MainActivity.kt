6daf64be14c719a0bb5247b5433eae6c90f5f464
dan-1010 <dhuliya.arpit@gmail.com>
2022-01-08 01:05:26 +0530

    Calculator first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.ArithmeticException
+
+class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot :Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View)
+    {
+        tvInput.append((view as Button).text)
+        lastNumeric=true
+    }
+
+    fun onClear(view: View)
+    {
+        tvInput.text=""
+        lastNumeric=false
+        lastDot=false
+    }
+    fun onDecimalPoint(view: View)
+    {
+        if (lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric=false
+            lastDot=true
+        }
+    }
+
+    fun onOperator(view: View)
+    {
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString()))
+            tvInput.append((view as Button).text)
+            lastNumeric= false
+            lastDot=false
+
+    }
+
+
+
+    fun onEqual(view: View)
+    {
+        if(lastNumeric){
+            var tvValue = tvInput.text.toString()
+            var prefix=""
+            try {
+                if (tvValue.startsWith("-")){
+                    prefix="-"
+                    tvValue=tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")){
+                    val splitvalue= tvValue.split("-")
+
+                    var one= splitvalue[0]
+                    var two=splitvalue[1]
+
+                    if(!prefix.isEmpty())
+                    {
+                        one=prefix+one
+                    }
+
+                    tvInput.text=removeZeroAfterDot((one.toDouble()-two.toDouble()).toString())
+
+                }
+                else if(tvValue.contains("+")){
+                    val splitvalue= tvValue.split("+")
+
+                    var one= splitvalue[0]
+                    var two=splitvalue[1]
+
+                    if(!prefix.isEmpty())
+                    {
+                        one=prefix+one
+                    }
+
+                    tvInput.text=removeZeroAfterDot((one.toDouble()+two.toDouble()).toString())
+
+                }
+                else if(tvValue.contains("*")){
+                    val splitvalue= tvValue.split("*")
+
+                    var one= splitvalue[0]
+                    var two=splitvalue[1]
+
+                    if(!prefix.isEmpty())
+                    {
+                        one=prefix+one
+                    }
+
+                    tvInput.text=removeZeroAfterDot((one.toDouble()*two.toDouble()).toString())
+
+                }
+                if(tvValue.contains("/")){
+                    val splitvalue= tvValue.split("/")
+
+                    var one= splitvalue[0]
+                    var two=splitvalue[1]
+
+                    if(!prefix.isEmpty())
+                    {
+                        one=prefix+one
+                    }
+
+                    tvInput.text=removeZeroAfterDot((one.toDouble()/two.toDouble()).toString())
+
+                }
+
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun removeZeroAfterDot(result: String):String{
+        var value= result
+        if(result.contains(".0"))
+            value=result.substring(startIndex = 0,result.length-2)
+        return value
+    }
+
+    private fun isOperatorAdded(value:String) : Boolean{
+
+        return if(value.startsWith("-"))
+            false
+        else{
+            value.contains("/")||value.contains("*")||value.contains("+")||value.contains("-")
+
+
+        }
+    }
+
+}
